6ab8d3575a52bfe58002f15b334687e147ad9f98
Jan Svec <svec1jan@gmail.com>
2020-04-12 17:59:34 +0200

    replaced condition needed for synch with collectionCondition - to avoid the need to use another selector for element

--- src/test/java/IssuesPage.java
+++ src/test/java/IssuesPage.java
@@ -1,4 +1,4 @@
-import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selectors.ByText;
 import com.codeborne.selenide.Selenide;
@@ -6,7 +6,6 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.Assert;
 
 import static com.codeborne.selenide.Selectors.byXpath;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class IssuesPage {
@@ -14,13 +13,12 @@ public class IssuesPage {
 //    private String allItemsInIssueListClass = "yt-issues-tree__node yt-hover-actions yt-gutter-left-0 yt-issues-tree__node_compact";
     private String allItemsInIssueListXpath = "/html/body/div[1]/div/div/div/yt-analytics-zone/yt-navigation-zone/yt-navigation/div[3]/div/div[2]/div[2]/div/yt-events-animator/yt-sortable-tree/div/yt-sortable-tree-node";
 
-    private SelenideElement allItemsInIssueList = $(byXpath(allItemsInIssueListXpath));
     private ElementsCollection allItemsInIssue = $$(byXpath(allItemsInIssueListXpath));
 
     public void isNewIssueListed(String issueName) {
         Selenide.refresh();
-        // needed fo synchronization - cannot be done with collection
-        allItemsInIssueList.shouldBe(Condition.visible);
+        // needed for synchronization; there should be at least the 1 that was created
+        allItemsInIssue.shouldBe(CollectionCondition.sizeGreaterThan(0));
         boolean isInList = false;
         for (SelenideElement es: allItemsInIssue){
             if (es.find(new ByText(issueName)).exists()) {
